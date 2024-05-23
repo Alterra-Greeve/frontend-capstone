a0b6854f17e9fc979c1c521fc8269eb19983ef22
Kzquandary <kzquandary@gmail.com>
2024-05-23 19:02:24 +0700

    Init Project

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+    host: '0.0.0.0', 
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: '0.0.0.0', 
+  },
+})
