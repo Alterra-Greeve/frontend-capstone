eb095510a5b1b85cb149cc0cf8745aa14527763d
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2024-05-31 22:59:44 +0700

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import "./App.css";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
+import { Routes, Route } from "react-router-dom";
+import Challenges from "./pages/Challenges/index"
+import Dashboard from "./pages/Dashboard/index"
+import Products from "./pages/Products/index"
+import Users from "./pages/Users/index"
 
 function App() {
   return (
     <>
-      <h1 className="text-[100px] bg-blue-700 rounded-lg text-white">
+      <Routes>
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/users" element={<Users/>}/>
+        <Route path="/products" element={<Products/>}/>
+        <Route path="/challenges" element={<Challenges/>}/>
+      </Routes>
+      {/* <h1 className="text-[100px] bg-blue-700 rounded-lg text-white">
         test tailwind css
       </h1>
-      <Button>test shadcn ui</Button>
+      <Button>test shadcn ui</Button> */}
     </>
   );
 }
