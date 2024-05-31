1fe3925a3a64dd6d42efe790811c0386278d7417
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2024-05-31 23:23:37 +0700

    add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Challenges from "./pages/Challenges/index"
 import Dashboard from "./pages/Dashboard/index"
 import Products from "./pages/Products/index"
 import Users from "./pages/Users/index"
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Dashboard/>}/>
         <Route path="/users" element={<Users/>}/>
