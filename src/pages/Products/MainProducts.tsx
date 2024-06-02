2faf7e23eade1fb86367627558c84c1b05d2120c
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2024-06-02 21:59:27 +0700

    (feat): searchbar component & button

---
+++ src/pages/Products/MainProducts.tsx
@@ -0,0 +1,7 @@
+export default function MainProducts() {
+    return (
+        <main className="">
+            
+        </main>
+    )
+};
---
+++ src/pages/Products/PaginationProducts.tsx
@@ -0,0 +1,7 @@
+export default function PaginationProducts() {
+    return (
+        <div>
+            
+        </div>
+    )
+};
