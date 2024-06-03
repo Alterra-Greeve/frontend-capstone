6f7b50e40b36fe69a99555b127592d45a152c732
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2024-06-03 18:25:57 +0700

    dummy table

--- src/pages/Products/TableProducts.tsx
+++ src/pages/Products/TableProducts.tsx
@@ -24,35 +24,61 @@ export default function TableProducts({data}) {
                         <TableHead className="text-neutral-900 text-[12px] font-[400] leading-[24px] py-[8px] px-[12px] min-w-[90px]">Stock</TableHead>
                         <TableHead className="text-neutral-900 text-[12px] font-[400] leading-[24px] py-[8px] px-[12px] min-w-[90px]">Coin</TableHead>
                         <TableHead className="text-neutral-900 text-[12px] font-[400] leading-[24px] py-[8px] px-[12px] min-w-[90px]">Description</TableHead>
-                        <TableHead className="text-neutral-900 text-[12px] font-[400] leading-[24px] py-[8px] px-[12px] min-w-[90px]">Helps</TableHead>
+                        <TableHead className="text-neutral-900 text-[12px] font-[400] leading-[24px] py-[8px] min-w-[90px]">Helps</TableHead>
                         <TableHead className="text-neutral-900 text-[12px] font-[400] leading-[24px] py-[8px] px-[12px] min-w-[90px]">Create Date</TableHead>
                         <TableHead className="text-neutral-900 text-[12px] font-[400] leading-[24px] py-[8px] px-[12px] min-w-[90px]">Update Date</TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    <TableRow>
+                    <TableRow className="bg-neutral-50">
                         <TableCell className="p-[12px]">#STRW-01-BLK</TableCell>
                         <TableCell className="p-[12px]">
-                            <img src={avatar} alt="" />
+                            <img src={avatar} alt="" className="w-[24px] h-[24px]"/>
                         </TableCell>
                         <TableCell className="p-[12px]">Sedotan Besi</TableCell>
                         <TableCell className="p-[12px]">Rp20.000,-</TableCell>
                         <TableCell className="p-[12px]">200</TableCell>
                         <TableCell className="p-[12px]">100</TableCell>
                         <TableCell className="p-[12px]">Sedotan besi adalah..</TableCell>
-                        <TableCell className="p-[12px]">
+                        <TableCell className="py-[12px]">
                             <div className="flex gap-[4px]">
-                                <img src={helps} alt="" />
-                                <img src={helps} alt="" />
-                                <img src={helps} alt="" />
-                                <img src={helps} alt="" />
+                                <img src={helps} alt="" className="w-[24px] h-[24px]"/>
+                                <img src={helps} alt="" className="w-[24px] h-[24px]"/>
+                                <img src={helps} alt="" className="w-[24px] h-[24px]"/>
+                                <img src={helps} alt="" className="w-[24px] h-[24px]"/>
                             </div>
                         </TableCell>
                         <TableCell className="p-[12px]">09/05/24</TableCell>
                         <TableCell className="p-[12px]">
                             <div className="flex gap-[12px]">
                                 <span>12/05/24</span>
-                                <img src={more} alt="" />
+                                <img src={more} alt="" className="w-[24px] h-[24px]"/>
+                            </div>
+                        </TableCell>
+                    </TableRow>
+                    <TableRow className="bg-neutral-200">
+                        <TableCell className="p-[12px]">#STRW-01-BLK</TableCell>
+                        <TableCell className="p-[12px]">
+                            <img src={avatar} alt="" className="w-[24px] h-[24px]"/>
+                        </TableCell>
+                        <TableCell className="p-[12px]">Sedotan Besi</TableCell>
+                        <TableCell className="p-[12px]">Rp20.000,-</TableCell>
+                        <TableCell className="p-[12px]">200</TableCell>
+                        <TableCell className="p-[12px]">100</TableCell>
+                        <TableCell className="p-[12px]">Sedotan besi adalah..</TableCell>
+                        <TableCell className="py-[12px]">
+                            <div className="flex gap-[4px]">
+                                <img src={helps} alt="" className="w-[24px] h-[24px]"/>
+                                <img src={helps} alt="" className="w-[24px] h-[24px]"/>
+                                <img src={helps} alt="" className="w-[24px] h-[24px]"/>
+                                <img src={helps} alt="" className="w-[24px] h-[24px]"/>
+                            </div>
+                        </TableCell>
+                        <TableCell className="p-[12px]">09/05/24</TableCell>
+                        <TableCell className="p-[12px]">
+                            <div className="flex gap-[12px]">
+                                <span>12/05/24</span>
+                                <img src={more} alt="" className="w-[24px] h-[24px]"/>
                             </div>
                         </TableCell>
                     </TableRow>
