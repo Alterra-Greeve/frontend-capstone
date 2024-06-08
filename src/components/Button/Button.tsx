3a91e5b219a87c63c05af56dce83160276cd596c
imkzuma <edu.gungkrisna@gmail.com>
2024-06-08 13:36:19 +0800

    (feat): FIX ALL BUG AND REFACTOR USERS FEATURE

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,4 @@
 type ButtonProps = {
-<<<<<<< HEAD
   variant: 'primary' | 'secondary';
   icon?: React.ReactNode;
   children: React.ReactNode;
@@ -21,28 +20,4 @@ export default function Button({ variant, icon, children, onClick }: ButtonProps
       {children}
     </button>
   )
-=======
-    variant: 'primary' | 'secondary';
-    icon?: any;
-    children: React.ReactNode;
-    className?: any;
-    onClick?: () => void
-};
-
-export default function Button({variant, icon, children, onClick, className} : ButtonProps) {
-    let style: any;
-    if(variant === 'primary'){
-        style = 'bg-primary-500 text-neutral-100 border-primary-500 hover:bg-primary-600 hover:shadow-custom active:bg-primary-700'
-    }else{
-        style = 'bg-transparent text-primary-500 border-primary-500 hover:shadow-custom active:bg-primary-50'
-    }
-    return (
-        <button className={`${style}
-            flex gap-[4px] justify-center items-center border-solid border-[1px] 
-            p-[8px] font-[500] text-[14px] rounded-[8px] ${className}`} onClick={onClick}>
-                <div className={icon? "w-[24px] h-[24px]" : "hidden"}>{icon}</div>
-                {children}
-        </button>
-    )
->>>>>>> b841e0d80dc9c9ffea5135f72706acb6e7ab070d
-};
+}
