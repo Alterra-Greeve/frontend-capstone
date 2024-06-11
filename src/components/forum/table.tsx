9f0d21daa35f524cc9e13642bf018f1cee54daff
BangSann <yosanokta12@gmail.com>
2024-06-12 00:17:51 +0700

    fix: :bug: add validation in edit users

--- src/components/forum/table.tsx
+++ src/components/forum/table.tsx
@@ -8,15 +8,30 @@ import {
 } from "@/components/ui/table";
 import { useAppSelector } from "@/lib/redux";
 import MoreIcon from "@/assets/icons/More.svg";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+import DeleteIcon from "@/assets/icons/Iconly/Union-1.svg";
+import ShowProfileIcon from "@/assets/icons/Iconly/Show.svg";
+import { useNavigate } from "react-router-dom";
 
 const ForumTable = () => {
   const { discussions = [] } = useAppSelector((state) => state.forum);
+  const navigate = useNavigate();
 
   return (
     <div className="mt-4 bg-primary-100 rounded-t-[8px] border-[1px] border-neutral-300">
       <Table>
         <TableHeader>
           <TableRow className="text-start py-[10px]">
+            <TableHead
+              className={`text-sm leading-5 text-black font-normal px-3 py-2`}
+            >
+              No
+            </TableHead>
             <TableHead
               className={`text-sm leading-5 text-black font-normal px-3 py-2`}
             >
@@ -47,6 +62,7 @@ const ForumTable = () => {
                 index % 2 !== 0 ? "bg-neutral-200" : ""
               }`}
             >
+              <TableCell className="p-3 text-start">{index + 1}</TableCell>
               <TableCell className="p-3 text-start">{item.title}</TableCell>
               <TableCell className="p-3 text-start">
                 {item.description}
@@ -54,20 +70,39 @@ const ForumTable = () => {
               <TableCell className="p-3 text-start">
                 {item.author.name}
               </TableCell>
-              <TableCell className="flex gap-2 p-3 text-start">
-                <div className="w-6 rounded-full bg-slate-300 border border-black">
-                  <img
-                    src={
-                      item.author.avatar_url ||
-                      "@/assets/images/default-user.png"
-                    }
-                    className="w-full rounded-full h-6"
-                  />
+              <TableCell className=" p-3 text-start">
+                <div className="flex gap-2">
+                  <div className="w-6 rounded-full bg-slate-300 border border-black">
+                    <img
+                      src={
+                        item.author.avatar_url ||
+                        "@/assets/images/default-user.png"
+                      }
+                      className="w-full rounded-full h-6"
+                    />
+                  </div>
+                  {item.author.name || "-"}
                 </div>
-                {item.author.name || "-"}
               </TableCell>
-              <TableCell className="max-w-6  p-3">
-                <MoreIcon />
+              <TableCell className="max-w-6 p-3 text-center pe-4">
+                <DropdownMenu>
+                  <DropdownMenuTrigger className="outline-none hover:bg-neutral-300 py-2 rounded-lg flex justify-center items-center">
+                    <MoreIcon />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="bg-white rounded-lg absolute right-5 w-36 -top-7 p-3 text-neutral-900 flex flex-col  gap-1 shadow-md">
+                    <DropdownMenuItem
+                      className="text-sm font-bold flex gap-2 p-2 hover:bg-neutral-100 hover:rounded-md outline-none cursor-pointer"
+                      onClick={() => navigate(`./${item.id}`)}
+                    >
+                      <ShowProfileIcon />
+                      Lihat
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="text-sm font-bold flex gap-2 p-2 hover:bg-neutral-100 hover:rounded-md outline-none cursor-pointer">
+                      <DeleteIcon />
+                      Hapus
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
               </TableCell>
             </TableRow>
           ))}
--- src/lib/redux/api/forum.ts
+++ src/lib/redux/api/forum.ts
@@ -8,6 +8,11 @@ interface Author {
   name: string;
   avatar_url: string;
 }
+interface ForumMessage {
+  id: string;
+  user_id: string;
+  message: string;
+}
 
 interface Discussion {
   id: string;
@@ -15,9 +20,17 @@ interface Discussion {
   description: string;
   author: Author;
 }
+interface DiscussionDetail {
+  id: string;
+  title: string;
+  description: string;
+  author: Author;
+  forum_messages: ForumMessage[];
+}
 
 interface ForumState {
   discussions: Discussion[];
+  discussionsDetail: DiscussionDetail;
   loading: boolean;
   error: string | null;
   status: boolean;
@@ -26,6 +39,18 @@ interface ForumState {
 
 const initialState: ForumState = {
   discussions: [],
+  discussionsDetail: {
+    id: "",
+    title: "",
+    description: "",
+    author: {
+      id: "",
+      username: "",
+      name: "",
+      avatar_url: "",
+    },
+    forum_messages: [],
+  },
   loading: false,
   error: null,
   status: false,
@@ -43,6 +68,40 @@ export const fetchDiscussions = createAsyncThunk(
     } catch (error) {
       if (error instanceof AxiosError) {
         throw error.response ? error.response.status : error.message;
+      }
+      throw error;
+    }
+  }
+);
+
+export const fetchDiscussionById = createAsyncThunk(
+  "forum/fetchDiscussionById",
+  async (forum_id: string) => {
+    try {
+      const response = await GreeveApi.get(`/forums/${forum_id}`);
+      if (response.status === 200) {
+        return response.data;
+      }
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        throw error.response ? error.response.data : error.message;
+      }
+      throw error;
+    }
+  }
+);
+
+export const deleteForumById = createAsyncThunk(
+  "forum/deleteForumById",
+  async (forum_id: string) => {
+    try {
+      const response = await GreeveApi.delete(`/forums/${forum_id}`);
+      if (response.status === 200) {
+        return forum_id; // Return the id of the deleted forum
+      }
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        throw error.response ? error.response.data : error.message;
       }
       throw error;
     }
@@ -69,6 +128,38 @@ export const forumSlice = createSlice({
         state.loading = false;
         state.status = false;
         state.message = "Get Forum Failed";
+      })
+      .addCase(fetchDiscussionById.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchDiscussionById.fulfilled, (state, action) => {
+        state.discussionsDetail = action.payload.data; // Correctly assigning the payload to discussionsDetail
+        state.loading = false;
+        state.status = true;
+        state.message = "Get Specific Forum Success";
+      })
+      .addCase(fetchDiscussionById.rejected, (state, action) => {
+        state.error = action.payload as string | null;
+        state.loading = false;
+        state.status = false;
+        state.message = "Get Specific Forum Failed";
+      })
+      .addCase(deleteForumById.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(deleteForumById.fulfilled, (state, action) => {
+        state.discussions = state.discussions.filter(
+          (discussion) => discussion.id !== action.payload
+        );
+        state.loading = false;
+        state.status = true;
+        state.message = "Delete Forum Success";
+      })
+      .addCase(deleteForumById.rejected, (state, action) => {
+        state.error = action.payload as string | null;
+        state.loading = false;
+        state.status = false;
+        state.message = "Delete Forum Failed";
       });
   },
 });
