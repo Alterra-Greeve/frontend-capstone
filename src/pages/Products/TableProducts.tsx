3acc2f0fcc64610ca37793559d87f37ef7be9d3e
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2024-06-16 01:23:25 +0700

    feat: filter products

--- src/pages/Products/TableProducts.tsx
+++ src/pages/Products/TableProducts.tsx
@@ -14,7 +14,7 @@ import CatRecycle from '@/assets/icons/catRecycle.svg'
 import ManageProducts from "./ManageProducts";
 import { useState } from "react";
 
-export default function TableProducts({ data, dataShow, filterValue, isFiltered}: any) {
+export default function TableProducts({ data, dataShow, filterValue, isFiltered }: any) {
     const [isOpen, setIsOpen] = useState(false)
     const [selectedDropDown, setSelectedDropDown] = useState("");
     const tableHeadStyle = "text-neutral-900 text-[12px] font-[400] leading-[24px] py-[8px] min-w-[90px]"
@@ -41,47 +41,95 @@ export default function TableProducts({ data, dataShow, filterValue, isFiltered}
                         <TableHead className={`${tableHeadStyle} px-[12px]`}>Update Date</TableHead>
                     </TableRow>
                 </TableHeader>
-                <TableBody>
-                    {data.data.map((item: any, i: any) => {
-                        if (i > dataShow.Start && i < dataShow.end) {
-                            return (
-                                <TableRow className={i % 2 === 0 ? 'bg-neutral-50' : 'bg-neutral-200'} key={i}>
-                                    <TableCell className="p-[12px]">{item.product_id}</TableCell>
-                                    <TableCell className="p-[12px]">
-                                        <img src={item.image_url[0]} className="w-[24px] h-[24px] rounded-[24px]"></img>
-                                    </TableCell>
-                                    <TableCell className="p-[12px]">{item.name}</TableCell>
-                                    <TableCell className="p-[12px]">{item.price}</TableCell>
-                                    <TableCell className="p-[12px]">{item.stock}</TableCell>
-                                    <TableCell className="p-[12px]">{item.coin}</TableCell>
-                                    <TableCell className="p-[12px]">{item.description}</TableCell>
-                                    <TableCell className="py-[12px]">
-                                        <div className="flex gap-[4px]">
-                                            {item.category.includes('b5d07366-3b31-4011-95e3-34735b0b61f8') ? <CatEarth /> : null}
-                                            {item.category.includes('83808762-e2b8-4b34-a1eb-0ed8d4fda3dd') ? <CatMoney /> : null}
-                                            {item.category.includes('e8e714bd-c34e-4278-980c-39bd1f55b5fb') ? <CatBrain /> : null}
-                                            {item.category.includes('7d34a5fa-e2cf-466d-9f01-d731f6967082') ? <CatRecycle /> : null}
-                                        </div>
-                                    </TableCell>
-                                    <TableCell className="p-[12px]">{item.created_at}</TableCell>
-                                    <TableCell className="p-[12px] relative">
-                                        <div className="flex gap-[12px] items-center">
-                                            <span>12/05/24</span>
-                                            <div className="w-[24px] h-[24px] cursor-pointer" onClick={() => handleOpen(item.product_id)}>
-                                                <SeeMore />
-                                            </div>
-                                        </div>
-                                        {selectedDropDown === item.product_id && (
-                                            <ManageProducts isOpen={isOpen} id={item.product_id} />
-                                        )}
-                                    </TableCell>
-                                </TableRow>
-
+                {isFiltered ?
+                    <TableBody>
+                        {
+                            data.data.map((item: any, i: any) => {
+                                if (i > dataShow.Start && i < dataShow.end) {
+                                    if (item.price >= filterValue?.minPrice && item.price <= filterValue?.maxPrice 
+                                        && item.stock >= filterValue?.minStock && item.stock <= filterValue?.maxStock
+                                        && item.coin >= filterValue?.minCoin && item.coin <= filterValue?.maxCoin) {
+                                        return (
+                                            <TableRow className={i % 2 === 0 ? 'bg-neutral-50' : 'bg-neutral-200'} key={i}>
+                                                <TableCell className="p-[12px]">{item.product_id}</TableCell>
+                                                <TableCell className="p-[12px]">
+                                                    <img src={item.image_url[0]} className="w-[24px] h-[24px] rounded-[24px]"></img>
+                                                </TableCell>
+                                                <TableCell className="p-[12px]">{item.name}</TableCell>
+                                                <TableCell className="p-[12px]">{item.price}</TableCell>
+                                                <TableCell className="p-[12px]">{item.stock}</TableCell>
+                                                <TableCell className="p-[12px]">{item.coin}</TableCell>
+                                                <TableCell className="p-[12px]">{item.description}</TableCell>
+                                                <TableCell className="py-[12px]">
+                                                    <div className="flex gap-[4px]">
+                                                        {item.category.includes('b5d07366-3b31-4011-95e3-34735b0b61f8') ? <CatEarth /> : null}
+                                                        {item.category.includes('83808762-e2b8-4b34-a1eb-0ed8d4fda3dd') ? <CatMoney /> : null}
+                                                        {item.category.includes('e8e714bd-c34e-4278-980c-39bd1f55b5fb') ? <CatBrain /> : null}
+                                                        {item.category.includes('7d34a5fa-e2cf-466d-9f01-d731f6967082') ? <CatRecycle /> : null}
+                                                    </div>
+                                                </TableCell>
+                                                <TableCell className="p-[12px]">{item.created_at}</TableCell>
+                                                <TableCell className="p-[12px] relative">
+                                                    <div className="flex gap-[12px] items-center">
+                                                        <span>12/05/24</span>
+                                                        <div className="w-[24px] h-[24px] cursor-pointer" onClick={() => handleOpen(item.product_id)}>
+                                                            <SeeMore />
+                                                        </div>
+                                                    </div>
+                                                    {selectedDropDown === item.product_id && (
+                                                        <ManageProducts isOpen={isOpen} id={item.product_id} />
+                                                    )}
+                                                </TableCell>
+                                            </TableRow>
+                                        )
+                                    }
+                                }
+                            }
                             )
                         }
-                    }
-                    )}
-                </TableBody>
+                    </TableBody>
+                    :
+                    <TableBody>
+                        {data.data.map((item: any, i: any) => {
+                            if (i > dataShow.Start && i < dataShow.end) {
+                                return (
+                                    <TableRow className={i % 2 === 0 ? 'bg-neutral-50' : 'bg-neutral-200'} key={i}>
+                                        <TableCell className="p-[12px]">{item.product_id}</TableCell>
+                                        <TableCell className="p-[12px]">
+                                            <img src={item.image_url[0]} className="w-[24px] h-[24px] rounded-[24px]"></img>
+                                        </TableCell>
+                                        <TableCell className="p-[12px]">{item.name}</TableCell>
+                                        <TableCell className="p-[12px]">{item.price}</TableCell>
+                                        <TableCell className="p-[12px]">{item.stock}</TableCell>
+                                        <TableCell className="p-[12px]">{item.coin}</TableCell>
+                                        <TableCell className="p-[12px]">{item.description}</TableCell>
+                                        <TableCell className="py-[12px]">
+                                            <div className="flex gap-[4px]">
+                                                {item.category.includes('b5d07366-3b31-4011-95e3-34735b0b61f8') ? <CatEarth /> : null}
+                                                {item.category.includes('83808762-e2b8-4b34-a1eb-0ed8d4fda3dd') ? <CatMoney /> : null}
+                                                {item.category.includes('e8e714bd-c34e-4278-980c-39bd1f55b5fb') ? <CatBrain /> : null}
+                                                {item.category.includes('7d34a5fa-e2cf-466d-9f01-d731f6967082') ? <CatRecycle /> : null}
+                                            </div>
+                                        </TableCell>
+                                        <TableCell className="p-[12px]">{item.created_at}</TableCell>
+                                        <TableCell className="p-[12px] relative">
+                                            <div className="flex gap-[12px] items-center">
+                                                <span>12/05/24</span>
+                                                <div className="w-[24px] h-[24px] cursor-pointer" onClick={() => handleOpen(item.product_id)}>
+                                                    <SeeMore />
+                                                </div>
+                                            </div>
+                                            {selectedDropDown === item.product_id && (
+                                                <ManageProducts isOpen={isOpen} id={item.product_id} />
+                                            )}
+                                        </TableCell>
+                                    </TableRow>
+                                )
+                            }
+                        }
+                        )}
+                    </TableBody>
+                }
             </Table>
         </div>
     )
