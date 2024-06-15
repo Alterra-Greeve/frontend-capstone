2de9d1c954732ef3bd562019f31de44ee78a3f4a
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2024-06-15 22:35:40 +0700

    .

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Download from "@/assets/icons/Export.svg"
 import Plus from "@/assets/icons/plus.svg"
