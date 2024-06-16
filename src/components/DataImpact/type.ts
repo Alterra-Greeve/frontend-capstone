fb77ff8a6fcdaccad7db445d1d795484fafc6ac8
imkzuma <edu.gungkrisna@gmail.com>
2024-06-17 00:59:00 +0800

    (feat): split type data impact

--- src/components/DataImpact/type.ts
+++ src/components/DataImpact/type.ts
@@ -1,12 +1,13 @@
+interface DataImpactHelpsProps {
+  impact_category: {
+    name: string;
+    image_url: string;
+  }
+}
 export interface DataImpactProps {
   username: string;
   challenge_name: string;
   email: string;
   impact_point: number;
-  helps: {
-    impact_category: {
-      name: string;
-      image_url: string;
-    }
-  }[]
+  helps: DataImpactHelpsProps[];
 }
