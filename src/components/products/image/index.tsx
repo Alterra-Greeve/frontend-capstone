8756fd3a38f2dd0b2e7605ce5f2bd91e0498a52b
imkzuma <edu.gungkrisna@gmail.com>
2024-06-20 15:54:01 +0800

    (change): fixed select image bug, and improve ui

--- src/components/products/image/index.tsx
+++ src/components/products/image/index.tsx
@@ -12,6 +12,7 @@ import EmptyPhotoSmall from '@/assets/icons/EmptyPhotoSmall.svg'
 import PlusIconSmall from '@/assets/icons/PlusPhotoSmall.svg'
 
 import CloseIconSmall from '@/assets/icons/CloseIconSmall.svg'
+import { nanoid } from '@reduxjs/toolkit';
 
 const validationImage = (file: File) => {
   const validImageTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/webp'];
@@ -45,8 +46,8 @@ export const ShowBigImage = ({ image, title, onDelete }: ShowImageProps) => (
 )
 
 export const ShowChildImage = ({ image, title, onDelete }: ShowImageProps) => (
-  <div className="relative bg-neutral-50 border-1 border border-[#17171712] rounded-xl max-w-32 max-h-24">
-    <img src={image} alt={title} className="w-full object-cover rounded-xl" />
+  <div className="relative bg-neutral-50 border-1 border border-[#17171712] rounded-xl max-w-full max-h-24">
+    <img src={image} alt={title} className="object-cover rounded-xl w-full h-24" />
     <span
       className='absolute top-[0.5px] right-[1px] cursor-pointer rounded-2xl'
       onClick={onDelete}
@@ -60,18 +61,25 @@ export const SelectChildImage = ({ onSaveFile, disabled }: { onSaveFile: (file: 
   const dispatch = useAppDispatch();
 
   const fileRef = useRef<HTMLInputElement | null>(null);
-  const onSelectImage = () => fileRef.current?.click();
+  const onSelectImage = () => !disabled ? fileRef.current?.click() : null;
 
   const { toast } = useToast();
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
+    const fileInput = e.target;
+    const file = fileInput.files?.[0];
+
     try {
       if (!file) {
         throw new Error('File not found');
       }
+
       validationImage(file);
-      const urlImage = URL.createObjectURL(file!);
+
+      const fileExtension = file.name.split('.').pop();
+      const uniqueFileName = `${nanoid()}.${fileExtension}`;
+
+      const urlImage = URL.createObjectURL(new File([file], uniqueFileName, { type: file.type }));
 
       dispatch(setTempImage(urlImage));
       onSaveFile(file);
@@ -89,14 +97,16 @@ export const SelectChildImage = ({ onSaveFile, disabled }: { onSaveFile: (file: 
         variant: "destructive",
         description: "Failed to upload image"
       });
+    } finally {
+      fileInput.value = '';
     }
-  }
+  };
 
 
   return (
     <div
       onClick={onSelectImage}
-      className={`${disabled ? "bg-neutral-100" : "border-[2px] border-dashed border-primary-500 bg-primary-50"} flex justify-center items-center rounded-xl w-full h-24 transition-all hover:cursor-pointer hover:bg-neutral-100`}>
+      className={`${disabled ? "bg-neutral-100 hover:cursor-not-allowed" : "hover:cursor-pointer border-[2px] border-dashed border-primary-500 bg-primary-50"} flex justify-center items-center rounded-xl w-full h-24 transition-all hover:bg-neutral-100`}>
 
       <div className={`relative ${disabled ? "hidden" : "block"}`}>
         <EmptyPhotoSmall />
